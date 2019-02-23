fa401d334477bace824df70b95fd633730e476f1
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-23 15:10:37 +0900

    Qiita 用の出力をする

--- ruby/table.rb
+++ ruby/table.rb
@@ -1,6 +1,6 @@
 require_relative "is_int.rb"
 
-Funcs = %i(
+Funcs_to_i = %i(
   int_floor?
   int_ceil?
   int_round?
@@ -27,7 +27,7 @@ results = {}
 results[:to_i]=Inputs.each.with_object({}){ |kv, o |
   name = kv[:name]
   value = kv[:value]
-  r = Funcs.map{ |func|
+  r = Funcs_to_i.map{ |func|
     begin
       method(func).call(value)
     rescue=>e
@@ -38,4 +38,28 @@ results[:to_i]=Inputs.each.with_object({}){ |kv, o |
   o[name]=r[0]
 }
 
-p results
+
+Funcs = [
+  { name:"int32(比較)", f: :is_int32_1 },
+  { name:"int32(pack/unpack)", f: :is_int32_2 },
+]
+
+Funcs.each do |name:, f:|
+  results[name]=Inputs.each.with_object({}){ |kv, o |
+    iname = kv[:name]
+    value = kv[:value]
+    r = begin
+        method(f).call(value)
+    rescue=>e
+      "例外"
+    end
+    o[iname]=r
+  }
+end
+
+
+puts( "|入力|"+results.keys.map{ |k| k.to_s+"|" }.join("") )
+puts("|:--|"+":--:|"*results.size)
+Inputs.each do |i|
+  puts("|"+i[:name]+"|"+results.values.map{ |v| "#{v[i[:name]]}|" }.join("") )
+end
